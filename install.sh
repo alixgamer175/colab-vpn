ee624a54f82d21ddbcfe6e270ee9a0e9fc6798ea
The AliX Legend <alixgamer175@gmail.com>
2021-11-30 15:31:07 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 apt update
 apt install ca-certificates wget net-tools gnupg -y
 wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
+echo "deb http://as-repository.openvpn.net/as/debian bionic main">/etc/apt/sources.list.d/openvpn-as-repo.list
 echo "deb http://as-repository.openvpn.net/as/debian focal main">/etc/apt/sources.list.d/openvpn-as-repo.list
+echo "deb http://as-repository.openvpn.net/as/debian bullseye main">/etc/apt/sources.list.d/openvpn-as-repo.list
+echo "deb http://as-repository.openvpn.net/as/debian buster main">/etc/apt/sources.list.d/openvpn-as-repo.list
 apt update
 apt install openvpn-as -y
 mkdir -p /dev/net
